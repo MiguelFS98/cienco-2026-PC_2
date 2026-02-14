1ae8f6184dd8e55579d325f443db308826855b57
miguelfsilva <miguelfariassilva5@gmail.com>
2026-02-14 17:32:18 -0300

    Primeira Lista de Exercicios de Programação de Computadores 2

---
+++ Lista de Exercicios 1/0.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nota = 61;
+
+    if(nota >= 60)
+    {
+        cout << "Aprovado!";
+    }
+    else
+    {
+        cout << "Reprovado!";
+    }
+    return 0;
+}
